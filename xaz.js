f81aecc96d578a223567d0a15de396fd51d1d720
xiaoben8857 <kolendaedythk@gmail.com>
2025-03-06 23:14:41 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -1,9 +1,9 @@
  
 
-var xz = "/xiazai/Lets-VIP.zip";
+var xz = "https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip";
 
-//可以把   /xiazai/letspn-latest64.zip  这个改成下载地址；https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip
-//可以把   https://guoxiauzausj.oss-rg-china-mainland.aliyuncs.com/letspn-latest64.zip  这个改成文件下载：/xiazai/kuailian.zip
+//可以把   /xiazai/https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成下载地址；https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip
+//可以把   https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成文件下载：/xiazai/kuailian.zip
 
 
 
